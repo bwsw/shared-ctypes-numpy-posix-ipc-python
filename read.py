918a33bce7c6026d9249424cd7c1a516d5507625
Ivan A. Kudryavtsev <kudryavtsev_ia@bw-sw.com>
2019-12-03 16:01:25 +0700

    Improved read.py with FPS and dynamic sleep.

--- read.py
+++ read.py
@@ -1,6 +1,6 @@
 import cv2
 
-from time import sleep
+from time import sleep, time
 from shm.reader import SharedMemoryFrameReader
 
 
@@ -8,11 +8,19 @@ if __name__ == '__main__':
 
     shm_r = SharedMemoryFrameReader('frame')
 
+    fps = 25
+    max_sleep = 1.0 / 25
+    next_sleep = max_sleep
+
     while True:
-        sleep(0.03)
+        sleep(next_sleep)
+        begin = time()
         f = shm_r.get()
         cv2.imshow('frame', f)
         if cv2.waitKey(1) & 0xFF == ord('q'):
              break
+        end = time()
+        next_sleep = max_sleep - (end - begin)
+        next_sleep = 0 if next_sleep <= 0 else next_sleep
 
     shm_r.release()
